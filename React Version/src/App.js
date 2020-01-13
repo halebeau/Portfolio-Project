ebe120892159df31cbef3e6f30aee7c34197684e
Beau Hale <halebeau3@gmail.com>
2020-01-12 16:19:49 -0800

    firsst commit

---
+++ React Version/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Navbar dark color="primary">
+                <div className="container">
+                    <NavbarBrand href="/">NuCamp</NavbarBrand>
+                </div>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default App;
